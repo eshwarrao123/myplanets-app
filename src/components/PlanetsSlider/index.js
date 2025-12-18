37177449104f3273643b176e423162b41c5f2d1f
eshwarrao123 <erao2303@gmail.com>
2025-12-18 14:33:20 +0000

    Initial planets app commit

---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,25 @@
+// Write your code here
+// Write your code here
+import Slider from 'react-slick'
+import Snowfall from 'react-snowfall'
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetSlider = props => {
+  const {planetsList} = props
+
+  return (
+    <div className="planets-app-container">
+      <Snowfall color="white" />
+      <h1 className="heading">PLANETS</h1>
+      <Slider>
+        {planetsList.map(eachPlanet => (
+          <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetSlider
